8b719fdc04208690e23bf12711878afcb1d0c0fd
yohn-dezmon <38733157+yohn-dezmon@users.noreply.github.com>
2018-11-15 13:29:42 -0600

    Update die_git.py

--- die_git.py
+++ die_git.py
@@ -1,16 +1,16 @@
 import random
 import sys
 
-print("Sup boi? Would you like some dice to throw? (yes or no)")
+print("Sup friend? Would you like some dice to throw? (yes or no)")
 remark = input("> ")
 while remark.upper() == "YES":
     die1 = random.randint(1,6)
     die2 = random.randint(1,6)
-    print(f'''Yo boi, you've rolled a {die1} and a {die2}
+    print(f'''Yo brother, you've rolled a {die1} and a {die2}
     Dare ye roll once mo'?''')
     remark = input("> ")
 
 if remark.upper() == "NO":
-    print("well played, mi dearest boi boi")
+    print("well played, doggo!")
 else:
-    print("HEYO, LEARN HOW TO ANSWER DA QUESTION!")
+    print("HEYO, learn how to answer the question!")
