9fc20c096a3d0cd3f497847023e5ee6d6cdf144d
yohn-dezmon <38733157+yohn-dezmon@users.noreply.github.com>
2018-12-05 13:35:03 -0600

    Update json_practice.py

--- json_practice.py
+++ json_practice.py
@@ -42,42 +42,10 @@ hs.write_json()
 hs.load_json()
 hs.print_list()
 
-# -----------------------
-# my_list = [3,2,1]
-# json_list = json.dumps(my_list)
-#
-# # the indent just makes the list easier to read (if you have to read the list)
-#
-# # with opens the destination file!
-# #then the json.dumps writes the data object to the outfile as a string.
-# with open('/Users/HomeFolder/Python1/High_Scores/data.txt', 'w') as outfile:
-#     for x in json_list:
-#         outfile.write(x)
-#
-#
-# with open('/Users/HomeFolder/Python1/High_Scores/data.txt', 'r') as json_list:
-#     data = json.load(json_list)
-#     data.sort()
-#
-#
-# # for number in data:
-# #     print(number)
-#
-
-
-
 
 #------------
 # sources:
 # (1) https://www.youtube.com/watch?v=9N6a-VLBa2I
 # (2) https://stackabuse.com/reading-and-writing-json-to-a-file-in-python/
+# (3) https://stackoverflow.com/questions/21297533/python-open-a-json-file-in-a-different-directory
 
-# ok this is coool but how do I open it/write it when I want the file to come from
-# a different directory?
-# https://stackoverflow.com/questions/21297533/python-open-a-json-file-in-a-different-directory
-# I think I need to use open!
-
-# ok now I need to appepnd to the list!
-
-# problem: I can update the attribute list high_scores...
-# but when I call high_scores after that OH! it has to do with the instantiation!
